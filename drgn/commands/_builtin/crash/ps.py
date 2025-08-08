739fe89c1794a799e814101b64fe5c4fd1791226
Yassine Larhrissi <yassine.larhrissi@oracle.com>
2025-08-08 10:17:59 -0400

    Crash: add ps command
    Signed-off-by: Yassine Larhrissi <yassine.larhrissi@oracle.com>

--- drgn/commands/_builtin/crash/ps.py
+++ drgn/commands/_builtin/crash/ps.py
@@ -1,27 +1,28 @@
 # Copyright (c) 2025 Oracle and/or its affiliates
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-"""An implementation of ps(crash command) using drgn"""
+"""
+Implements the crash "ps" command for drgn.
+
+This command displays process status for selected, or all, processes
+in the system.
+"""
 
 import argparse
 from typing import Any, Iterable, Optional, Tuple
 
-import drgn
 from drgn import Object, Program
 from drgn.commands import argument, drgn_argument
 from drgn.commands.crash import crash_command
-from drgn.helpers.common.format import print_table
-from drgn.helpers.linux.mm import get_task_rss_info, get_task_vmem, totalram_pages
-from drgn.helpers.linux.percpu import per_cpu
-from drgn.helpers.linux.pid import (
-    find_task,
-    for_each_task,
-    get_command,
-    is_group_leader,
-    is_kthread,
-    is_user,
+from drgn.helpers.common.format import escape_ascii_string, print_table
+from drgn.helpers.linux.kthread import task_is_kthread
+from drgn.helpers.linux.mm import get_task_rss_info, task_vsize, totalram_pages
+from drgn.helpers.linux.pid import find_task, for_each_task, is_group_leader
+from drgn.helpers.linux.sched import (
+    task_cpu,
+    task_since_last_arrival_ns,
+    task_state_to_char,
 )
-from drgn.helpers.linux.sched import cpu_curr, task_cpu, task_state_to_char
 
 ByteToKB = 1024
 
@@ -46,16 +47,7 @@ ByteToKB = 1024
 # The following task states have same values in
 # all currently supported kernel versions.
 
-
-def nanosecs_to_secs(nanosecs: int) -> float:
-    """
-    Convert from nanosecs to secs
-
-    :param nanosecs: time duration in nano secs
-    :returns: time duration in secs
-    """
-    val = nanosecs // 1000000
-    return val / 1000
+_NSECS_TO_SECS = 1000000000
 
 
 def get_task_arrival_time(task: Object) -> int:
@@ -69,67 +61,15 @@ def get_task_arrival_time(task: Object) -> int:
     :returns: arrival time instance in ns granularity
     """
 
-    try:
-        arrival_time = task.last_run.value_()
-    except AttributeError:
-        try:
-            arrival_time = task.timestamp.value_()
-        except AttributeError:
-            arrival_time = task.sched_info.last_arrival.value_()
+    arrival_time = task.sched_info.last_arrival.value_()
     return arrival_time
-
-
-def get_current_run_time(prog: drgn.Program, cpu: int) -> int:
-    """
-    Get running duration of the current task on some cpu
-
-    :param prog: drgn program
-    :param cpu: cpu index
-    :returns: duration in ns granularity
-    """
-    return task_lastrun2now(cpu_curr(prog, cpu))
-
-
-def runq_clock(prog: drgn.Program, cpu: int) -> int:
-    """
-    Get clock of cpu runqueue ``struct rq``
-
-    :param prog: drgn program
-    :param cpu: cpu index
-    :returns: cpu runqueue clock in ns granularity
-    """
-    rq = per_cpu(prog["runqueues"], cpu)
-    return rq.clock.value_()
-
-
-def task_lastrun2now(task: drgn.Object) -> int:
-    """
-    Get the duration from task last run timestamp to now
-
-    The return duration will cover task's last run time on cpu and also
-    the time staying in current status, usually the time slice for task
-    on cpu will be short, so this can roughly tell how long this task
-    has been staying in current status.
-    For task status in "RU" status, if it's still on cpu, then this return
-    the duration time this task has been running, otherwise it roughly tell
-    how long this task has been staying in runqueue.
-
-    :param prog: drgn program
-    :param task: ``struct task_struct *``
-    :returns: duration in ns granularity
-    """
-    prog = task.prog_
-    arrival_time = get_task_arrival_time(task)
-    rq_clock = runq_clock(prog, task_cpu(task))
-
-    return rq_clock - arrival_time
 
 
 def format_nanosecond_duration(nanosecs: int) -> str:
     """
     :returns: conversion of nanoseconds to [dd hh:mm:ss.ms] format
     """
-    secs = nanosecs_to_secs(nanosecs)
+    secs = nanosecs / _NSECS_TO_SECS
     dd, rem = divmod(secs, 86400)
     hh, rem = divmod(rem, 3600)
     mm, secs = divmod(rem, 60)
@@ -141,13 +81,13 @@ def show_tasks_last_runtime(tasks: Iterable[Object]) -> None:
     Display task information in their last arrival order.
     """
     tasks = list(tasks)
-    tasks.sort(key=task_lastrun2now)
+    tasks.sort(key=task_since_last_arrival_ns)
     for t in tasks:
         cpu = str(task_cpu(t))
         pid = str(t.pid.value_())
         state = task_state_to_char(t)
-        command = get_command(t)
-        time_nanosec = task_lastrun2now(t)
+        command = escape_ascii_string(t.comm.string_())
+        time_nanosec = task_since_last_arrival_ns(t)
         last_arrival = format_nanosecond_duration(time_nanosec)
         print(
             f'[{last_arrival}] [{state}]  PID: {pid:<8} TASK: {t.value_()}  CPU: {cpu:<2}  COMMAND: "{command}"'
@@ -164,7 +104,7 @@ def show_tasks_last_runtime_timestamp(tasks: Iterable[Object]) -> None:
         cpu = str(task_cpu(t))
         pid = str(t.pid.value_())
         state = task_state_to_char(t)
-        command = get_command(t)
+        command = escape_ascii_string(t.comm.string_())
         last_arrival = str(get_task_arrival_time(t))
         print(
             f'[{last_arrival}] [{state}]  PID: {pid:<8} TASK: {t.value_()}  CPU: {cpu:<2}  COMMAND: "{command}"'
@@ -192,9 +132,13 @@ def show_taskinfo(prog: Program, tasks: Iterable[Object]) -> None:
                 hex(t.value_()),
                 task_state_to_char(t),
                 str("%.1f" % pct_mem),
-                str(get_task_vmem(t)),
+                str(task_vsize(t) // ByteToKB),
                 str(rss_kb),
-                f"[{get_command(t)}]",
+                (
+                    f"[{escape_ascii_string(t.comm.string_())}]"
+                    if task_is_kthread(t)
+                    else f"{escape_ascii_string(t.comm.string_())}"
+                ),
             ]
         )
     print_table(rows)
@@ -272,24 +216,19 @@ def _crash_cmd_ps(
     if args.drgn:
         print(
             """
-        from drgn.helpers.linux.pid import for_each_task, get_command
-        from drgn.helpers.linux.percpu import percpu_counter_sum
+        from drgn.helpers.linux.pid import for_each_task
         from drgn.helpers.common.format import escape_ascii_string
-        from drgn.helpers.mm import get_task_vmem
+        from drgn.helpers.linux.mm import task_vsize
 
+        total_mem = int(totalram_pages(prog))
+        page_size = prog["PAGE_SIZE"].value_()
         for task in for_each_task(prog):
-            total_mem = int(totalram_pages(prog))
-            page_size = prog["PAGE_SIZE"].value_()
-            pid = task.pid.value_()
-            ppid = task.parent.pid.value_()
-            cpu = task.cpu.value_()
-            task = hex(task.value_())
             state = task_state_to_char(task)
-            task_rss = get_task_rss_info(t)
+            task_rss = get_task_rss_info(task)
             total_rss = task_rss.total
             pct_mem = total_rss * 100 / total_mem
-            vmem = get_task_vmem(task)
-            command = get_command(task)
+            vmem = task_vsize(task)
+            command = escape_ascii_string(task.comm.string_())
         """
         )
         return
@@ -301,12 +240,14 @@ def _crash_cmd_ps(
         elif args.arg[0] == "task":
             tasks = iter([Object(prog, "struct task_struct *", value=args.arg[1])])
         elif args.arg[0] == "comm":
-            tasks = (t for t in tasks if get_command(t) == args.arg[1])
+            tasks = (
+                t for t in tasks if escape_ascii_string(t.comm.string_()) == args.arg[1]
+            )
 
     if args.user:
-        tasks = filter(is_user, tasks)
+        tasks = filter(lambda t: not task_is_kthread(t), tasks)
     elif args.kernel:
-        tasks = filter(is_kthread, tasks)
+        tasks = filter(task_is_kthread, tasks)
     if args.group_leader:
         tasks = filter(is_group_leader, tasks)
     if args.last_run:
